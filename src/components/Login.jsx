fd4a9982307b68e058d119b8a6f83e4f8d24b58d
muskan <muskanshaikh20004@gmail.com>
2024-04-13 13:40:12 +0530

    login ui

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,6 +6,8 @@ import { MDBContainer, MDBInput } from "mdb-react-ui-kit";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import "../allCss/Login.css";
+import login from "../assets/login.png";
 
 const Login = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
@@ -58,65 +60,78 @@ const Login = () => {
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "30vw",
-      }}
-    >
-      <MDBContainer
-        className="p-2 my-4 d-flex flex-column w-100"
-        style={{
-          boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.3)",
-          borderRadius: "15px",
-        }}
-      >
-        <label
-          style={{
-            fontSize: "18px",
-            fontWeight: "500",
-            color: "#333",
-            marginBottom: "8px",
-            paddingLeft: "10px",
-          }}
-        >
-          Login to continue
-        </label>
-
-        <div style={{ position: "relative" }}>
-          <span
-            style={{
-              position: "absolute",
-              left: "10px",
-              top: "7px",
-              zIndex: 1,
-            }}
-          >
-            +91
-          </span>
-          <MDBInput
-            wrapperClass="mb-4"
-            id="form2"
-            type="text"
-            style={{ paddingLeft: "40px" }}
-            onKeyDown={handleKeyDown}
-            maxLength={10}
-            minLength={10}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-          />
+    <>
+      <body>
+        <div className="left-img-div">
+          <div className="img-div">
+            <img src={login} alt="Login Img" />
+          </div>
         </div>
+        <div className="login-div">
+          <div className=" login-div-box">
+            <MDBContainer
+              className="p-4  d-flex flex-column w-100 "
+              style={{
+                boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
+                borderRadius: "15px",
+                height: "30vh",
+              }}
+            >
+              <label
+                style={{
+                  fontSize: "18px",
+                  fontWeight: "500",
+                  color: "#333",
+                  marginBottom: "8px",
+                  paddingLeft: "10px",
+                }}
+              >
+                Login to continue
+              </label>
 
-        <button
-          className="mb-4"
-          style={{ color: "blue", borderRadius: "15px", padding: "5px" }}
-          onClick={submitLogin}
-        >
-          Login in
-        </button>
-      </MDBContainer>
-    </div>
+              <div style={{ position: "relative" }}>
+                <span
+                  style={{
+                    position: "absolute",
+                    left: "10px",
+                    top: "7px",
+                    zIndex: 1,
+                  }}
+                >
+                  +91
+                </span>
+                <MDBInput
+                  wrapperClass="mb-4"
+                  id="form2"
+                  type="text"
+                  style={{ paddingLeft: "40px" }}
+                  placeholder="Enter Your Number"
+                  onKeyDown={handleKeyDown}
+                  maxLength={10}
+                  minLength={10}
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                />
+              </div>
+
+              <button
+                className="mb-3"
+                style={{
+                  color: "white",
+                  borderRadius: "5px",
+                  padding: "5px",
+                  backgroundColor: "#438eff",
+                  fontWeight: "500",
+                }}
+                onClick={submitLogin}
+              >
+                Login
+              </button>
+            </MDBContainer>
+          </div>
+        </div>
+      </body>
+    </>
   );
 };
 
--- src/components/UploadCategory.jsx
+++ src/components/UploadCategory.jsx
@@ -3,15 +3,30 @@ import axios from "axios";
 
 const UploadCategory = () => {
   const [cat_name, setCatName] = useState("");
+  const [Catimg, setCatimg] = useState([]);
+
+  const handleImageChange = (e) => {
+    const files = Array.from(e.target.files);
+    setCatimg([...Catimg, ...files]);
+  };
 
   const handleSubmit = async () => {
     try {
-      if (!cat_name) {
+      console.log("Categories Images:", Catimg);
+      if (!cat_name || !Catimg.length) {
         return window.alert("Please fill all the information");
       }
 
       const formData = new FormData();
       formData.append("name", cat_name);
+
+      console.log("Before map, images:", Catimg);
+
+      // Append images to formData using map
+      const imageFiles = Catimg.map((image) => {
+        formData.append("Categpry Images", image);
+        return image;
+      });
 
       await axios.post("https://onestore-vert.vercel.app/category", formData, {
         headers: {
@@ -21,37 +36,54 @@ const UploadCategory = () => {
 
       window.alert("Category added successfully");
       setCatName(""); // Clear input field after successful submission
+      setCatimg([]);
+      window.location.reload();
     } catch (error) {
       console.error("Error submitting form:", error);
     }
   };
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6">
-          <h5>Enter Category Name</h5>
-          <input
-            type="text"
-            className="form-control"
-            value={cat_name}
-            onChange={(e) => setCatName(e.target.value)}
-          />
+    <>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <h5>Enter Category Name</h5>
+            <input
+              type="text"
+              className="form-control"
+              value={cat_name}
+              onChange={(e) => setCatName(e.target.value)}
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="row mt-3">
-        <div className="col-md-6">
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={handleSubmit}
-          >
-            Upload
-          </button>
+        <div className="row mt-3">
+          <div className="col-md-6">
+            <h5>Upload Category Images</h5>
+            <input
+              type="file"
+              className="form-control"
+              accept="image/jpeg, image/png"
+              multiple
+              onChange={handleImageChange}
+            />
+          </div>
+        </div>
+
+        <div className="row mt-3">
+          <div className="col-md-6">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={handleSubmit}
+            >
+              Upload
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
